5ff96ecd91035885d22a7e3916ca613574e5d1d3
Sekarmano <s.manojbe2012@gmail.com>
2019-09-20 18:37:04 +0530

    New codes are update

--- src/org/mk/conflict/CloneConflict.java
+++ src/org/mk/conflict/CloneConflict.java
@@ -2,7 +2,10 @@ package org.mk.conflict;
 
 public class CloneConflict {
 	private void loginId() {
-		System.out.println("manoj@95");		
+		System.out.println("manoj@95");	
+		}
+	private void click() {
+		System.out.println("Click login button");
 
 	}
 	
